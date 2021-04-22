ab28453abec97815c7f2cda2e20389a6d7658cc6
jonnycross10 <crossjonny5@gmail.com>
2021-04-22 00:34:18 -0700

    assignments is now a class that interfaces with main

--- assignments.py
+++ assignments.py
@@ -1,43 +1,57 @@
 import requests
 import json
 import dateutil.parser
-from datetime import *
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import pytz
 
 '''
 Throughout this API, the :user_id parameter can be replaced with self as a shortcut for the id of the user accessing the API.
 For instance, users/:user_id/page_views can be accessed as users/self/page_views to access the current user's page views.
 '''
 
-apiKey = "**********************"
+'''
+apiKey = "11299~LMYUCn8olYHTl1soCXfp9yAA4ybJ46aY2OgZ2IGsFrN4yBQ23j6ZC6BMAAOTYcr0"
 
 headers = {
   "Authorization":"Bearer "+apiKey
 }
+'''
+class Courses(object):
+	def __init__(self, url, apiKey):
+		self.url = url
+		self.apiKey = apiKey
 
-r = requests.get("https://csus.instructure.com/api/v1/courses", headers = headers)
 
-data = r.json()
+	def classData(self):
+		headers = { "Authorization":"Bearer "+self.apiKey}
 
-#print(data)
-#print(data[]["name"])
-def findRightClasses(d):#return actual classes
-	correctClass = []
-	for i in range(0,len(d)):
-		if (len(d[i].keys()) > 10):
-			correctClass.append(d[i])
-	return(correctClass)
+		r = requests.get(self.url, headers = headers)
 
-def thisTerm(d): #return classes from this term
-	correctClass = []
-	
-	
-	fiveMonthsAgo = datetime.now() - date(0000,5,00)
-	for i in range(0,len(d)):
-		createdDate = dateutil.parser.parse(d[i]["created_at"])
-		if(createdDate>fiveMonthsAgo):
-			correctClass.append(d[i])
-	return(correctClass)
+		data = r.json()
 
-#created at date within the last 5 months, pop if not
+		#print(data)
+		#print(data[]["name"])
+		def currentClasses(d):
+			def findRightClasses(d):#return actual classes
+				correctClass = []
+				for i in range(0,len(d)):
+					if (len(d[i].keys()) > 10):
+						correctClass.append(d[i])
+				return(correctClass)
 
-print(thisTerm(findRightClasses(data)))
+			def thisTerm(d): #return classes from this term
+				correctClass = []
+				utc = pytz.UTC 
+				fiveMonthsAgo = utc.localize(datetime.now() + relativedelta(months=-5))
+				for i in range(0,len(d)):
+					startDate = dateutil.parser.parse(d[i]["start_at"])
+					if(startDate>fiveMonthsAgo): #if start date was sooner than 5 months ago, it was this term
+						correctClass.append(d[i])
+				return(correctClass)
+			return(thisTerm(findRightClasses(d)))
+		return(currentClasses(data))
+
+		#created at date within the last 5 months, pop if not
+
+		#print(currentClasses(data))
